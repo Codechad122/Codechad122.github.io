fa06eb03ab0953f34ad5465c2647ba686daae9b0
Codechad122 <footballers737@gmail.com>
2024-11-17 10:58:56 -0600

    more work on header and footer components

--- m011/components/footer.js
+++ m011/components/footer.js
@@ -5,9 +5,29 @@ class CustomFooter extends HTMLElement {
 
     connectedCallback(){
         const shadow = this.attachShadow({ mode: "open" });
-        const tag = document.createElement("p")
-        tag.innerText = "HELLO I AM TEST"
-        shadow.appendChild(tag)
+        const footerContainer = document.createElement("div")
+        footerContainer.setAttribute("class", "footer")
+
+        const footerTextContainer = document.createElement("p")
+        footerTextContainer.innerText = "azenner@art.edu" 
+
+        footerContainer.appendChild(footerTextContainer)
+
+        const style = document.createElement("style");
+        style.textContent = `
+            .footer {
+                display: flex;
+                flex-direction: column;
+                text-align: center;
+                margin-left: auto;
+                margin-right: auto;
+                padding-left: 16px;
+                padding-right: 16px;
+                width: 100%;
+            }
+        `
+        shadow.appendChild(style)
+        shadow.appendChild(footerContainer)
     }
 }
 
--- m011/components/header.js
+++ m011/components/header.js
@@ -4,10 +4,52 @@ class CustomHeader extends HTMLElement {
     };
 
     connectedCallback(){
+        const navListItems = ["Logo", "About", "Services", "News", "Contact", "Team"]
+
         const shadow = this.attachShadow({ mode: "open" });
-        const tag = document.createElement("p")
-        tag.innerText = "HELLO I AM TEST"
-        shadow.appendChild(tag)
+        const headerContainer = document.createElement("div")
+        headerContainer.setAttribute("class", "headerNav")
+        const headerNav = document.createElement("nav")
+        const navList = document.createElement("ul")
+
+        headerNav.appendChild(navList)
+        headerContainer.appendChild(headerNav)
+
+        for(let i = 0; i < navListItems.length; i++){
+            let navItem = document.createElement('li')
+            let navLink = document.createElement('a')
+            navLink.href = `${navListItems[i].toLowerCase()}.html`
+            navItem.appendChild(navLink)
+            navLink.innerText = navListItems[i]
+            navList.appendChild(navItem)
+        }
+        
+        const style = document.createElement("style")
+        style.textContent = `
+            .headerNav {
+                display: flex;
+                justify-content: center;
+                margin-left: auto;
+                margin-right: auto;
+                padding-left: 16px;
+                padding-right: 16px;
+                width: 100%;
+            }
+
+            .headerNav nav ul {
+                list-style-type: none;
+                display: flex;
+                justify-content: center;
+                gap: 16px;
+            }
+
+            .headerNav nav ul li a {
+                color: black;
+                text-decoration: none;
+            }
+        `
+        shadow.appendChild(style)
+        shadow.appendChild(headerContainer)
     }
 }
 
